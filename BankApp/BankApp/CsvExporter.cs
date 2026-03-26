0cce9979b405a834e66534eb4ed6c7d1e87c00c7
Rihab-bou <57831751+Rihab-bou@users.noreply.github.com>
2026-03-26 13:24:18 +0100

    Add files via upload

---
+++ BankApp/BankApp/CsvExporter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace BankApp
+{
+    public class CsvExporter
+    {
+        public void Export(string filePath, List<Transaction> transactions)
+        {
+            using var writer = new StreamWriter(filePath);
+
+            writer.WriteLine("Date;Montant;Devise;Categorie");
+
+            foreach (var t in transactions)
+            {
+                writer.WriteLine($"{t.Date:yyyy-MM-dd};{t.Amount};{t.Currency};{t.Category}");
+            }
+        }
+    }
+}
---
+++ BankApp/BankApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using BankApp;
+using System.Security.Principal;
+using System.Transactions;
+using Transaction = BankApp.Transaction;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var account = new Account();
+        Seed(account);
+
+        var exporter = new CsvExporter();
+        var feature = new ExportFeature(account, exporter);
+
+        feature.Execute();
+    }
+
+    static void Seed(Account account)
+    {
+        account.AddTransaction(new Transaction(new DateTime(2022, 1, 10), 1000, Currency.EUR, Category.Salary));
+        account.AddTransaction(new Transaction(new DateTime(2022, 2, 15), -50, Currency.EUR, Category.Food));
+        account.AddTransaction(new Transaction(new DateTime(2022, 3, 01), -20, Currency.USD, Category.Transport));
+        account.AddTransaction(new Transaction(new DateTime(2022, 6, 1), 200, Currency.EUR, Category.Other));
+        account.AddTransaction(new Transaction(new DateTime(2023, 1, 20), -100, Currency.EUR, Category.Leisure));
+    }
+} 
